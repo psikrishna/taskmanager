55086c761299b1162b56248557dd3aa98a5d5e25
psikrishna <attalurisaikrishna@gmail.com>
2020-06-30 23:26:39 +0530

    using mongoose part 2 (user and task model set-up)

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,22 +6,42 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true,
 });
 
-const User = mongoose.model('User', {
-    name: {
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//     },
+//     age: {
+//         type: Number,
+//     },
+// });
+
+// const newUser = new User({
+//     name: 'sai',
+//     age: 'vingt-une'
+// });
+
+// newUser.save().then((newUser) => {
+//     log(newUser);
+// }).catch((error) => {
+//     log('Error!', error);
+// });
+
+const Task = mongoose.model('Task', {
+    description: {
         type: String,
     },
-    age: {
-        type: Number,
+    completed: {
+        type: Boolean,
     },
 });
 
-const newUser = new User({
-    name: 'sai',
-    age: 21
+const newTask = new Task({
+    description: 'learn mongoose',
+    completed: false,
 });
 
-newUser.save().then((newUser) => {
-    log(newUser);
+newTask.save().then((newTask) => {
+    log(newTask);
 }).catch((error) => {
-    log('Error!', error);
+    log(error);
 });
