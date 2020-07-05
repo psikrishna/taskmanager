f015813e6c636d93b8e8f5b95c11a011dfeb0b84
psikrishna <attalurisaikrishna@gmail.com>
2020-07-05 16:29:25 +0530

    validator added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const log = console.log;
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
@@ -9,22 +10,38 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
     name: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('age must be a positive number');
+            }
+        },
     },
+    email: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('enail invalid');
+            }
+        }
+    }
 });
 
-// const newUser = new User({
-//     name: 'sai',
-//     age: 'vingt-une'
-// });
+const newUser = new User({
+    name: 'test3',
+    age: 21,
+    email: 'test3@email',
+});
 
-// newUser.save().then((newUser) => {
-//     log(newUser);
-// }).catch((error) => {
-//     log('Error!', error);
-// });
+newUser.save().then((newUser) => {
+    log(newUser);
+}).catch((error) => {
+    log('Error!', error);
+});
 
 const Task = mongoose.model('Task', {
     description: {
@@ -35,13 +52,13 @@ const Task = mongoose.model('Task', {
     },
 });
 
-const newTask = new Task({
-    description: 'learn mongoose',
-    completed: false,
-});
+// const newTask = new Task({
+//     description: 'learn mongoose.',
+//     completed: false,
+// });
 
-newTask.save().then((newTask) => {
-    log(newTask);
-}).catch((error) => {
-    log(error);
-});
+// newTask.save().then((newTask) => {
+//     log(newTask);
+// }).catch((error) => {
+//     log(error);
+// });
