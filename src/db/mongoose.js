c3ec47ee165562d24670a6f623f14912dd70404c
psikrishna <attalurisaikrishna@gmail.com>
2020-07-05 22:15:41 +0530

    set up postman

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 const log = console.log;
 
 // initialise connection with db
@@ -7,60 +6,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
 });
-
-// user table
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('age must be a positive number');
-            }
-        },
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('enail invalid');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"');
-            }
-        }
-    }
-});
-
-// add user
-// const newUser = new User({
-//     name: 'test6',
-//     age: 21,
-//     email: 'test6@email.com',
-//     password: 'PaSsWoRdd',
-// });
-
-// newUser.save().then((newUser) => {
-//     log(newUser);
-// }).catch((error) => {
-//     log('Error!', error);
-// });
 
 // task table
 const Task = mongoose.model('Task', {
@@ -73,16 +18,4 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         default: false,
     },
-});
-
-// add task
-const newTask = new Task({
-    description: 'learn mongoose + validator',
-    completed: true,
-});
-
-newTask.save().then((newTask) => {
-    log(newTask);
-}).catch((error) => {
-    log(error);
 });
---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user');
+const log = console.log;
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// automatically parse incoming json to be used as an object
+app.use(express.json());
+
+// normally we use get to access the route using get http, but we are trying to use post for resource creation therefore we use app.post
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+    user.save().then(() => {
+        res.send(user)
+    }).catch(() => {
+
+    })
+});
+
+
+
+app.listen(port, () => {
+    log('Server is up on port' + port)
+});
