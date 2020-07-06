53286e6196add26caf1b77dde29006b86ebe2adc
psikrishna <attalurisaikrishna@gmail.com>
2020-07-06 22:46:00 +0530

    read user(s)

--- src/index.js
+++ src/index.js
@@ -47,7 +47,17 @@ app.get('/users', (req, res) => {
 
 // route for returning individual users by id
 app.get('/users/:id', (req, res) => {
-    User.find()
+    const _id = req.params.id
+    // findOne => to be used if searching by any other value say email etc
+    // findById to find by id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    });
 });
 
 // =================== U of CRUD ===================
