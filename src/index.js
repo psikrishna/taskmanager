ff4b3ff33cdecc20074ab0731de6693e554e1de0
psikrishna <attalurisaikrishna@gmail.com>
2020-07-08 01:39:16 +0530

    async-await into users

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
-const express = require('express');
-require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
-const log = console.log;
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
+const log = console.log
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
 // automatically parse incoming json to be used as an object
-app.use(express.json());
+app.use(express.json())
 
 // =================== C of CRUD ===================
 
 // normally we use get to access the route using get http, but we are trying to use post for resource creation therefore we use app.post
 // call to set up resource creation endpoint (user)
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e)
-    })
-});
+    }
+})
 
 // call to set up resource creation endpoint (task)
 app.post('/tasks', (req, res) => {
