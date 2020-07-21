55ba51b65a3fa71ea0c739d9c33866f1b21fce6b
psikrishna <attalurisaikrishna@gmail.com>
2020-07-21 18:42:40 +0530

    heroku deployement complete (backend)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const taskRouter = require('./routers/task')
 const log = console.log
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 // automatically parse incoming json to be used as an object
 app.use(express.json())
