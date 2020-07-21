ca24a3e2bf0c39a5ad254f8ef2ed8cd782744528
psikrishna <attalurisaikrishna@gmail.com>
2020-07-21 16:01:20 +0530

    welcome and cancel email set up

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,15 +4,35 @@ const sendgridAPIKey = 'SG.znxAJxPsTu6T0A31m5KZLA.aNYxZexJf4Er-I-p3JxfBfT_0Xk6uy
 
 sgMail.setApiKey(sendgridAPIKey)
 
-const msg = {
-    to: 'attalurisaikrishna@gmail.com',
-    from: 'attalurisaikrishna@gmail.com',
-    subject: 'sample',
-    text: 'sample text sent using node and sendgrid'
+const sendWelcomeEmail = (email, name) => {
+    // const msg = 
+    sgMail.send({
+        to: email,
+        from: 'attalurisaikrishna@gmail.com',
+        subject: 'Welcome to the Task Manager App',
+        text: `Welcome to the app, ${name}. Your feedback is important to us, so do let us know how you get along with the application.`
+    }).then(() => {
+        log('message sent to user')
+    }).catch((error) => {
+        log(error.response.body)
+    })
 }
 
-sgMail.send(msg).then(() => {
-    log('message sent')
-}).catch((e) => {
-    log(e.response.body)
-})
+const sendCancellationEmail = (email, name) => {
+    // const msg = 
+    sgMail.send({
+        to: email,
+        from: 'attalurisaikrishna@gmail.com',
+        subject: 'Thank you for using the Task Manager App',
+        text: `Goodbye, ${name}. Sorry to see you go, I hoep to see you back sometime soon.`
+    }).then(() => {
+        log('message sent to user')
+    }).catch((error) => {
+        log(error.response.body)
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancellationEmail,
+}
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,12 +4,14 @@ const sharp = require('sharp')
 const router = new express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendCancellationEmail } = require('../emails/account')
 
 // route to create user (sign-up)
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
@@ -78,6 +80,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
+        sendCancellationEmail(req.user.email, req.user.name)
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
